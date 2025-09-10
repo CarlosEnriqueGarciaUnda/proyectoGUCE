3161becad2a5dce4e9158bbc52959d92e4042da4
Garcia Unda Carlos Enrique <23160911@itoaxaca.edu.mx>
2025-09-10 12:10:54 -0600

    Segundo commit

---
+++ src/Ejemplo3.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Ejemplo3 {
+    private JPanel panelPrincipal;
+    private JPanel panelContenedor;
+    private JTextField passwordTextField;
+    private JCheckBox rememberMeCheckBox;
+    private JButton singInButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
