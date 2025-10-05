e7ba0ec48e4956458891bae23ba2ce564bd25bd8
Garcia Unda Carlos Enrique <23160911@itoaxaca.edu.mx>
2025-10-05 13:56:04 -0600

    Commit Actulizado

---
+++ src/LogIn.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import javax.swing.event.AncestorListener;
+
+public class LogIn {
+    private JPanel panel_principal;
+    private JPanel panel_segundo;
+    private JPanel panel_tercero;
+    private JLabel login;
+    private JTextField emailAddressTextField;
+    private JCheckBox rememberMeCheckBox;
+    private JButton singInButton;
+    private JLabel imagen;
+
+
+    public LogIn() {
+        panel_principal.addAncestorListener(new AncestorListener() {
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
